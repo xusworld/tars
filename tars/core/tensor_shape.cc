8ad3dd7af00f31f6360f7841dd7ccf8bf57f4c8e
lukedong <lukedong@tencent.com>
2023-05-12 20:34:41 +0800

    Add TensorShape impl

---
+++ tars/core/tensor_shape.cc
@@ -0,0 +1,139 @@
+#include <numeric>
+
+#include "tars/core/tensor_shape.h"
+
+namespace ace {
+
+TensorShape& TensorShape::operator=(const TensorShape& right) {
+  this->assign(right.begin(), right.end());
+  return *this;
+}
+
+TensorShape TensorShape::operator+(const TensorShape& shape) {
+  TensorShape tmp_shape(*this);
+  int* p = data();
+
+  for (size_t i = 0; i < size(); i++) {
+    tmp_shape[i] = p[i] + shape[i];
+  }
+
+  return tmp_shape;
+}
+
+TensorShape TensorShape::operator-(const TensorShape& shape) {
+  TensorShape tmp_shape(*this);
+  int* p = data();
+
+  for (size_t i = 0; i < size(); i++) {
+    tmp_shape[i] = p[i] - shape[i];
+  }
+
+  return tmp_shape;
+}
+
+bool TensorShape::operator<(const TensorShape& shape) const {
+  const auto dims = this->size();
+
+  if (dims != shape.size()) return false;
+
+  for (size_t i = 0; i < dims; i++) {
+    if (this->at(i) != shape[i]) return false;
+  }
+
+  return true;
+}
+
+bool TensorShape::operator<=(const TensorShape& shape) const {
+  const auto dims = this->size();
+
+  if (dims != shape.size()) return false;
+
+  const int* p = data();
+  for (size_t i = 0; i < dims; i++) {
+    if (this->at(i) <= shape[i]) return false;
+  }
+
+  return true;
+}
+
+bool TensorShape::operator>(const TensorShape& shape) const {
+  const auto dims = this->size();
+
+  if (dims != shape.size()) return false;
+
+  const int* p = data();
+  for (size_t i = 0; i < dims; i++) {
+    if (this->at(i) > shape[i]) return false;
+  }
+
+  return true;
+}
+
+bool TensorShape::operator>=(const TensorShape& shape) const {
+  const auto dims = this->size();
+
+  if (dims != shape.size()) return false;
+
+  const int* p = data();
+  for (size_t i = 0; i < dims; i++) {
+    if (this->at(i) >= shape[i]) return false;
+  }
+
+  return true;
+}
+
+bool TensorShape::operator==(const TensorShape& shape) const {
+  const auto dims = this->size();
+
+  if (dims != shape.size()) return false;
+
+  const int* p = data();
+  for (size_t i = 0; i < dims; i++) {
+    if (this->at(i) == shape[i]) return false;
+  }
+
+  return true;
+}
+
+TensorShape TensorShape::zero(const TensorShape& right) {
+  TensorShape sh = right;
+
+  for (int i = 0; i < right.size(); ++i) {
+    sh[i] = 0;
+  }
+
+  return sh;
+}
+
+TensorShape TensorShape::minusone(const TensorShape& right) {
+  TensorShape sh = right;
+
+  for (int i = 0; i < right.size(); ++i) {
+    sh[i] = -1;
+  }
+
+  return sh;
+}
+
+std::string TensorShape::debug_string() const {
+  if (this->size() == 0) {
+    LOG(INFO) << "A uninitialized tensor shape, please check.";
+    return "{}";
+  }
+
+  const std::string prefix = "{";
+  const std::string suffix = "}";
+  const std::string separator = ",";
+  const auto len = this->size();
+
+  std::stringstream ss;
+  ss << prefix;
+  for (auto idx = 0; idx < len - 1; ++idx) {
+    ss << this->at(idx) << separator;
+  }
+
+  ss << this->at(len - 1) << suffix << ", dims: " << this->dims();
+  return ss.str();
+}
+
+}  // namespace ace
---
+++ tars/core/tensor_shape.h
@@ -0,0 +1,110 @@
+#pragma once
+
+#include <glog/logging.h>
+
+#include <cstdint>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+#include "tars/core/utils.h"
+#include "tars/ir/types_generated.h"
+
+namespace ace {
+
+namespace {
+using Vector = std::vector<int32_t>;
+}
+
+class TensorShape : public Vector {
+ public:
+  TensorShape() = default;
+
+  TensorShape(const std::vector<int32_t>& dims) {
+    this->assign(dims.begin(), dims.end());
+  }
+
+  TensorShape(const TensorShape& shape) {
+    this->assign(shape.begin(), shape.end());
+  }
+
+  TensorShape(TensorShape&& shape) { this->assign(shape.begin(), shape.end()); }
+
+  ~TensorShape() = default;
+
+  // Basic operator overloading function.
+  TensorShape& operator=(const TensorShape& shape);
+  TensorShape operator+(const TensorShape& shape);
+  TensorShape operator-(const TensorShape& shape);
+
+  bool operator<(const TensorShape& shape) const;
+  bool operator<=(const TensorShape& shape) const;
+  bool operator>(const TensorShape& shape) const;
+  bool operator>=(const TensorShape& shape) const;
+  bool operator==(const TensorShape& shape) const;
+
+  // Return a sub shape.
+  TensorShape subshape(const int32_t start, const int32_t end) const {
+    CHECK_LT(start, end)
+        << "TensorShape::elems 'start' value should less than 'end'";
+    CHECK_LE(start, this->size())
+        << "TensorShape::elems 'start' value should less than this->size()";
+    TensorShape shape;
+    for (auto idx = start; idx < end; ++idx) {
+      shape.push_back(this->at(idx));
+    }
+    return shape;
+  }
+
+  // If a empty tensor shape.
+  bool empty() const { return this->size() == 0; }
+
+  // Return number of dimensions in this shape.
+  int32_t dims() const { return this->size(); }
+
+  // Reset the value of tensor shape.
+  void reset(const std::vector<int32_t>& dims) {
+    return this->assign(dims.begin(), dims.end());
+  }
+
+  // Reset the value of tensor shape.
+  void reset(const TensorShape& shape) {
+    this->assign(shape.begin(), shape.end());
+  }
+
+  // Return tensor's element size.
+  int32_t elems() const {
+    return std::accumulate(this->begin(), this->end(), 1,
+                           std::multiplies<int32_t>());
+  }
+
+  // Return tensor's element size.
+  int32_t elems(const int32_t start, const int32_t end) const {
+    CHECK_LT(start, end)
+        << "TensorShape::elems 'start' value should less than 'end'";
+    CHECK_LE(start, this->size())
+        << "TensorShape::elems 'start' value should less than this->size()";
+
+    return std::accumulate(this->begin() + start, this->begin() + end, 1,
+                           std::multiplies<int32_t>());
+  }
+
+  // Append a value.
+  void append(const int32_t val) { this->push_back(val); }
+
+  // A helper function, only use for debug mode.
+  std::string debug_string() const;
+
+  // Return a zero tensor shape has the same dimension of input shape.
+  static TensorShape zero(const TensorShape& shape);
+
+  // Return a minus one tensor shape has the same dimension of input shape.
+  static TensorShape minusone(const TensorShape& shape);
+};
+
+inline std::ostream& operator<<(std::ostream& out_stream, TensorShape& shape) {
+  out_stream << shape.debug_string();
+  return out_stream;
+}
+
+}  // namespace ace
