837f7a2c6f49cf6bdd56d2fc7c1683a5f62a3f0e
lukedong <lukedong@tencent.com>
2023-05-31 18:58:26 +0800

    add tile op shape inference

--- tars/core/shape/tile.cc
+++ tars/core/shape/tile.cc
@@ -9,7 +9,15 @@
 namespace tars {
 namespace core {
 
-// Concatenates tensors along one dimension.
+/* Constructs a tensor by tiling a given tensor.
+ *
+ * This operation creates a new tensor by replicating input multiples times. The
+ * output tensor's i'th dimension has input.dims(i) * multiples[i] elements, and
+ * the values of input are replicated multiples[i] times along the 'i'th
+ * dimension.
+ *
+ * For example, tiling [a b c d] by [2] produces [a b c d a b c d].
+ */
 class TileShapeInfer : public ShapeInfer {
  public:
   DISABLE_COPY_MOVE_ASSIGN(TileShapeInfer);
@@ -19,19 +27,44 @@ class TileShapeInfer : public ShapeInfer {
   // inputs shape --> outputs shape
   virtual Status run(const tars::Op* op, const std::vector<Tensor*>& inputs,
                      std::vector<Tensor*>& outputs) {
-    DLOG(INFO) << "CastShapeInfer...";
-    // check inputs number and outputs number
-    CHECK(inputs.size() >= 2)
-        << ", concat ops should have more than two inputs.";
-    CHECK(outputs.size() == 1) << ", concat ops shoule have one output.";
+    DLOG(INFO) << "Tile op shape inference ...";
+    // check inputs and outputs number
+    CHECK(inputs.size() == 2) << ", tile ops should have two inputs.";
+    CHECK(outputs.size() == 1) << ", tile ops shoule have one output.";
 
+    auto input = inputs[0];
+    auto multiples = inputs[1];
+
+    // check inputs' attributes
+    CHECK(input->rank() == multiples->size())
+        << ", input's rank should equal to multiples' element size";
+    CHECK(multiples->dtype() == DataType_DT_INT32)
+        << ", multiples tensor should be a int32 tensor.";
+    CHECK(multiples->rank() == 1)
+        << ", multiples tensor should be a 1-D tensor.";
+
+    // inference the shape of output
+    TensorShape shape;
+    for (int i = 0; i < multiples->size(); ++i) {
+      shape[i] = input->shape()[i] * multiples->data<int32_t>()[i];
+    }
+
+    // set output's tensor shape
+    outputs[0]->reshape(shape);
+    // set output's data type
+    outputs[0]->astype(input->dtype());
     // set output's data format
     outputs[0]->set_dformat(inputs[0]->dformat());
-    // set output's tensor shape
-    outputs[0]->reshape(inputs[0]->shape());
-    // set output's data type
-    const auto opParam = op->main_as_CastParam();
-    outputs[0]->set_dtype(opParam->dstT());
+
+    // debug log
+    std::vector<int32_t> multiples_vec;
+    for (int i = 0; i < multiples->size(); ++i) {
+      multiples_vec.push_back(multiples->data<int32_t>()[i]);
+    }
+
+    DLOG(INFO) << "input's shape: " << input->shape();
+    DLOG(INFO) << "multiples's content: " << multiples_vec;
+    DLOG(INFO) << "output's shape: " << outputs[0]->shape();
 
     return Status::OK();
   }
